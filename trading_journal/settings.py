14a9d0635af15622ad6b22ea6bf8350a889eedb5
BigBrandon <archbrandonkiprotich@gmail.com>
2025-10-16 15:53:50 +0300

    Add .gitignore and update settings.py for new apps and media configuration

--- trading_journal/settings.py
+++ trading_journal/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'accounts',
+    'trades',
+    'analytics',
+    'rest_framework',
+
 ]
 
 MIDDLEWARE = [
@@ -120,3 +126,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],  
+}

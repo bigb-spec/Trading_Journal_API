593ac61ae68c918e359db3d9cec7745d55382c9c
BigBrandon <archbrandonkiprotich@gmail.com>
2025-10-16 13:55:11 +0300

    Removed all  files and configurations for the trading journal project, so i can start a fresh as i was beginning to get confused and nothing seemed to be working

--- trades/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import Trade
-
-class TradeSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Trade
-        fields = '__all__'
-        read_only_fields = ['profit_loss', 'rr_ratio', 'day_of_week']
--- trading_journal/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-class TradingJournalConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'trading_journal'
